5f7394bc2828e6b77ebdb4c0f5c5a6b0be356f17
Yashaswi Bathula <bathula.yashaswi@gmail.com>
2025-09-13 21:27:53 -0700

    repo logic and mcp server integration

--- langgraph/demo.py
+++ langgraph/demo.py
@@ -1,10 +1,24 @@
 from langchain_openai import ChatOpenAI
 from langgraph.prebuilt import create_react_agent
 from langgraph_supervisor import create_supervisor
+from .repo_analysis import repo_analyzer_simple
+from mcp_client import read_project_file_mcp, print_conversation_summary
 
 def repo_analyzer(repo_name: str):
-    """Book a hotel"""
-    return f"Successfully booked a stay at {repo_name}."
+    """
+        Will Parse through the repos in a directory location, analyzing the file structure and dependencies.
+
+    """
+    result_json = repo_analyzer_simple()
+    return f"The overview of the projects within the file system: {result_json}."
+
+def mcp_file_summary(filename=None, projectname=None):
+    """
+        Use thi when you need to learn more about a file in the local repository.
+        Use this to get details from the file summarized and return here.
+
+    """
+    read_project_file_mcp(file_name=filename,project_name=projectname )
 
 def query_directory_users(team_name: str):
     """Book a flight"""
@@ -17,9 +31,13 @@ def query_directory_team(team_name: str):
 
 repo_assistant = create_react_agent(
     model="openai:gpt-4o",
-    tools=[repo_analyzer],
-    prompt="You are a flight booking assistant",
-    name="flight_assistant"
+    tools=[repo_analyzer, mcp_file_summary],
+    prompt="You are a repository analysis specialist.\n"
+        "Given the output from the `repo_analyzer` tool, infer the project purpose, architecture, "
+        "key directories, notable files, and how dependencies shape the stack.\n"
+        "Always call `repo_analyzer` first when given a repo URL. Return a concise, structured report."
+    ,
+    name="repo_assistant"
 )
 
 directory_assistant = create_react_agent(
